2f68265ea8308e5d90c385fa16ade116801fe8dd
yantingguang <ytg@yantinguangsMBP.lan>
2018-06-23 16:40:11 +0800

    添加elementUI，assets目录

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,11 +1,14 @@
 <template>
-  <div>我是首页</div>
+  <div class="index-control">
+    <div class="inner">
+      <h3>前端工程师能力评估</h3>
+    </div>
+  </div>
 </template>
 <script>
-export default {
-
-}
+export default {}
 </script>
 <style lang="scss" scoped>
-
+.index-control {
+}
 </style>
