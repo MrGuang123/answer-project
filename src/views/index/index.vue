e8b93ba424e7cf027567b994f10627f7a3e8d6b4
yantingguang <ytg@yantinguangsMBP.lan>
2018-06-23 17:29:38 +0800

    index complete, cancel eslint

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -2,13 +2,62 @@
   <div class="index-control">
     <div class="inner">
       <h3>前端工程师能力评估</h3>
+      <ul class="introduce">
+        <li>客观题：20道</li>
+        <li>完成时间：120分钟</li>
+        <li>难度系数：✨✨✨</li>
+        <li>总分：100分</li>
+      </ul>
+      <el-row class="btn-group">
+        <el-button type="primary" @click="begin">开始答题</el-button>
+        <el-button type="success">管理题目</el-button>
+      </el-row>
     </div>
   </div>
 </template>
 <script>
-export default {}
+export default {
+  methods: {
+    begin() {
+      this.$route.push({
+        name: 'answer'
+      })
+    }
+  }
+}
 </script>
 <style lang="scss" scoped>
 .index-control {
+  height: 100vh;
+  min-height: 500px;
+  display: flex;
+  align-items: center;
+  .inner {
+    width: 300px;
+    margin: 0 auto;
+    padding: 10px;
+    box-sizing: border-box;
+    h3 {
+      font-size: 26px;
+      padding-left: 20px;
+      margin-bottom: 40px;
+      line-height: 46px;
+      background: linear-gradient(0deg, #e92322, #e92322);
+      background-size: 10px 10px;
+      background-repeat: no-repeat;
+      background-position: 5px 18px;
+    }
+    .introduce {
+      margin-bottom: 40px;
+      li {
+        line-height: 24px;
+        padding-left: 10px;
+      }
+    }
+    .btn-group {
+      display: flex;
+      justify-content: space-around;
+    }
+  }
 }
 </style>
