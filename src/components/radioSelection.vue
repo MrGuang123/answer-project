dff4b5e66945c942d48a166ddc64de495951f33c
bianyanran <yeran220@163.com>
2018-06-23 17:51:02 +0800

    answer组件
    answer单选组件 radioSelection

---
+++ src/components/radioSelection.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="radio-box">
+    <p class="questText">{{radioTitle}}</p>
+    <el-radio-group v-model="radio">
+      <el-radio :label="3">备选项</el-radio>
+      <el-radio :label="6">备选项</el-radio>
+      <el-radio :label="9">备选项</el-radio>
+    </el-radio-group>
+  </div>
+
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        radio: '',
+        radioTitle: "下列哪个样式定义后,内联(非块状)元素可以定义宽度和高度"
+      };
+    }
+  }
+</script>
+<style>
+  .radio-box .el-radio__input.is-checked .el-radio__inner {
+    border-color: green;
+    background: green;
+  }
+
+  .el-radio__input.is-checked+.el-radio__label {
+    color: green;
+  }
+
+</style>
+
+<style scoped>
+  .questText {
+    margin: 46px 20px 12px 20px;
+  }
+
+  .el-radio-group {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .el-radio {
+    margin: 14px;
+    border: 1px solid #D3D3D3;
+    padding: 20px;
+  }
+</style>
--- src/views/answer/answer.vue
+++ src/views/answer/answer.vue
@@ -1,11 +1,60 @@
 <template>
-  <div>我是问题页</div>
+  <div class="answer-content">
+    <div class="answer-box">
+      <span class="typeCont">{{questype}}</span>
+      <radioSelection></radioSelection>
+      <div class="btn">
+        <button>下一题</button>
+        <button>提交</button>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-export default {
-
-}
+  import radioSelection from '../../components/radioSelection.vue'
+  export default {
+    data() {
+      return {
+        questype: '单选题'
+      }
+    },
+    components: {
+      radioSelection
+    }
+  }
 </script>
-<style lang="scss" scoped>
+<style>
+</style>
 
+<style scoped>
+  .answer-box {
+    width: 90%;
+    height: 500px;
+    /* box-shadow: 0px 0px 1px 1px #EEEEEE; */
+    margin: 30px auto;
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+
+  .typeCont {
+    display: inline-block;
+    width: 100%;
+    border-bottom: 1px solid #EEEEEE;
+    padding: 20px 0;
+  }
+
+  .btn {
+    display: flex;
+    flex-direction: row-reverse;
+    margin: 20px;
+
+  }
+
+  .btn button {
+    background-color: #FC6500;
+    margin-left: 6px;
+    padding: 10px 20px;
+    color: white;
+    border-radius: 6px;
+  }
 </style>
